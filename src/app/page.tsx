1da4062960ca089a76b909968f9fce6dc58f5538
TarekAmmar1993 <tarak.ammar@hotmail.com>
2025-02-28 14:49:26 +0100

    remove unnecesary imports and files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <h1 className="text-red-600">Hello World !</h1>
-  );
+  return <h1 className="text-red-600 mx-auto w-fit">Hello World !</h1>;
 }
